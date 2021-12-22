84341c9615ce27b1dd345bd51b8940d6ae243704
egonlr <egonlr@hotmail.com>
2021-12-22 10:46:44 -0300

    Controle de fluxo e paradigma procedural

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,45 @@
 fun main() {
     println("Bem vindo ao Bytebank")
-    val titular: String = "Egon"
-    val numeroConta: Int = 1000
-    var saldo: Double = 0.0
-    saldo = 100 + 2.0
-    saldo += 200
+    var i = 0
+    while (i < 5) {
+        i++
 
-    println("Titular: $titular")
-    println("Número da conta: $numeroConta")
-    println("Saldo: $saldo")
+//    for (i in 5 downTo 1) {
+//
+        if (i == 4) {
+            break
+        }
+        val titular: String = "Egon $i"
+        val numeroConta: Int = 1000
+        var saldo: Double = 0.0
+//    saldo = 100 + 2.0
+//    saldo += 200
+        saldo = 0.0 + i *10
+
+        println("Titular: $titular")
+        println("Número da conta: $numeroConta")
+        println("Saldo: $saldo")
+        println()
+//
+      }
+
+//    testaCondicoes(saldo)
+
+}
+
+fun testaCondicoes(saldo: Double) {
+
+    if (saldo > 0.0) {
+        println("Conta é positiva")
+    } else if (saldo == 0.0) {
+        println("Conta é neutra")
+    } else {
+        println("Conta é negativa")
+    }
+
+    when {
+        saldo > 0.0 -> println("Conta é positiva")
+        saldo == 0.0 -> println("Conta é neutra")
+        else -> println("Conta é negativa")
+    }
 }
