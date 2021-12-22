811232d9f934402fc1e1a31cd2259f9e9ece22cf
egonlr <egonlr@hotmail.com>
2021-12-22 09:54:03 -0300

    Primeiros passos - Kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("Bem vindo ao Bytebank")
+    val titular: String = "Egon"
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
+
+    println("Titular: $titular")
+    println("Número da conta: $numeroConta")
+    println("Saldo: $saldo")
+}
